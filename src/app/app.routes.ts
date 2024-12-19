d192c69724f10e6aa0d1c174a9e0495e74ebea9b
feng <zzhxufeng@163.com>
2024-12-19 22:48:51 +0800

    remove home route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,5 @@ import { Routes } from '@angular/router';
 import { BlogPreviewCardComponent } from './blog-preview-card/blog-preview-card.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: BlogPreviewCardComponent },
+  { path: '', component: BlogPreviewCardComponent },
 ];
